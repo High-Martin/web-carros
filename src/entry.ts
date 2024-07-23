e92690882a45c01c8d34e0d3281269168f3ee525
Bruno Martins <bgmartins@ucs.br>
2024-07-22 23:04:46 -0300

    feat: :bug: Expondo pasta de assets e public para o servidor node.js.

--- src/entry.ts
+++ src/entry.ts
@@ -5,9 +5,12 @@ import path from 'path';
 const app = express();
 const port = 3000;
 
+// Servir arquivos estáticos da pasta 'assets'
+app.use('/assets', express.static(path.join(__dirname, '../assets')));
+app.use('/public', express.static(path.join(__dirname, '../public')));
 
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname + '/src/index.html'));
+  res.sendFile(path.join(__dirname, '../public', 'index.html'));
 });
 
 app.listen(port, () => {
